fc07911ab371ed8a1da9f89b551cd1d9bb6e27c7
Paulo Mariano <paulotiagocm@hotmail.com>
2019-06-21 06:43:43 -0300

    manifest website

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(<App />, document.querySelector('#app'))
+
+serviceWorker.register()
