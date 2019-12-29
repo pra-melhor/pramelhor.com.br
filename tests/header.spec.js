759be55fdb90f603b36f1729e7a6c814f6526f9a
Paulo Tiago <paulotiagocm@hotmail.com>
2019-12-29 17:23:28 +0000

    tests header

---
+++ tests/header.spec.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Header from "../src/components/Header";
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders with or without a name", () => {
+  act(() => {
+    render(<Header />, container);
+  });
+  expect(container.textContent).toMatch(/Pra melhor.*/);
+});
